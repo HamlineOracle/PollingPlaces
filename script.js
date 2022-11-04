b18a53553e480fd8d5de5d00bb87428f1510bedc
HamlineOracle <117468256+HamlineOracle@users.noreply.github.com>
2022-11-04 18:41:34 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
 //    <script src="https://cdn.plot.ly/plotly-2.16.1.min.js"></script>
 
-let w = Window.innerWidth
-let h = Window.innerHeight
+hamlineCoord = [[[-93.1637176774289, 44.9633693933355],
+[-93.1637166745899, 44.9636068082378],
+[-93.1638259510017, 44.9636058667381],
+[-93.1642887474383, 44.9636020986195],
+[-93.1642875870184, 44.9640678779952],
+[-93.1670472064697, 44.9640493346361],
+[-93.1670535983955, 44.9660156728897],
+[-93.1678968687665, 44.9660134464492],
+[-93.1679025537774, 44.9664556250093],
+[-93.1677531380449, 44.9664562480592],
+[-93.1677450536507, 44.9668587192654],
+[-93.1670371197845, 44.9668572573513],
+[-93.1670558580918, 44.9687523366148],
+[-93.1629704661066, 44.9687918852697],
+[-93.1607989195329, 44.9685804740049],
+[-93.1607960542088, 44.9678134572746],
+[-93.1607972017574, 44.9677131035545],
+[-93.1623155422094, 44.9676908488288],
+[-93.1627401230231, 44.9676873872315],
+[-93.1627511967473, 44.967569562307],
+[-93.1627386292548, 44.9673035507754],
+[-93.1623128593523, 44.9673084515439],
+[-93.162207390041, 44.9673096653263],
+[-93.1621943556282, 44.9629020420632],
+[-93.162516076493, 44.9629026650579],
+[-93.1625201501839, 44.9633766869296],
+[-93.1635914562844, 44.9633689290027],
+[-93.1637176774289, 44.9633693933355]],
+[[[-93.1637177845602, 44.96334399118],
+[-93.1637176774289, 44.9633693933355],
+[-93.1637149575595, 44.9628897413582],
+[-93.1640581176166, 44.9628877918327],
+[-93.1640578413832, 44.9629958226544],
+[-93.1640578315224, 44.9633408362207],
+[-93.1637177845602, 44.96334399118]]]]
+
+h1lat = []
+h1lon = []
+h2lat = []
+h2lon = []
+
+for (let i=0;i<hamlineCoord[0].length;i++) {
+    h1lon.push(hamlineCoord[0][i][0])
+    h1lat.push(hamlineCoord[0][i][1])
+}
+for (let i=0;i<hamlineCoord[1].length;i++) {
+    h2lon.push(hamlineCoord[1][i][0])
+    h2lat.push(hamlineCoord[1][i][1])
+}
+
 
 d3.csv('RamseyPollingPlaces.csv', function(error, data) {
     d = {}
@@ -20,6 +68,25 @@ d3.csv('RamseyPollingPlaces.csv', function(error, data) {
         d['Text'].push(d['Name'][i] + '<br>'+d['Address'][i] + ' ' + d['Zip'][i])
     }
 
+    let hancock = {}
+    hancock['Name'] = [d['Name'][125]]
+    hancock['Address'] = [d['Address'][125]]
+    hancock['Zip'] = [d['Zip'][125]]
+    hancock['lat'] = [d['lat'][125]]
+    hancock['lon'] = [d['lon'][125]]
+    hancock['Text'] = [d['Text'][125]]
+
+
+    k = Object.keys(d)
+
+    for (let i=0;i<k.length;i++) {
+        o = k[i]
+        console.log(d[o][125])
+        d[o][125] = d[o][124]
+
+    }
+
+
     graphing = []
 
     for (let i=0;i<d['Name'].length;i++) {
@@ -35,12 +102,15 @@ d3.csv('RamseyPollingPlaces.csv', function(error, data) {
 
     data2 = []
 
-    tposOptions = ['top right', 'bottom right']
+    tposOptions = ['top right', 'top left']
     tpos = []
     for (let i=0;i<d['Name'].length;i++) {
-        tpos.push(tposOptions[i%2])
+        if (i != 125) {
+            tpos.push(tposOptions[0])
+        } else {
+            tpos.push(tposOptions[1])
+        }
     }
-
     data2.push({
         type: 'scattermapbox',
         mode: 'markers+text',
@@ -59,8 +129,93 @@ d3.csv('RamseyPollingPlaces.csv', function(error, data) {
 
         },
         text: d['Text'],
-        textposition: 'top right',
+        textposition: tpos[0]
     })
+
+    data2.push({
+        type: 'scattermapbox',
+        mode: 'markers+text',
+        hoverinfo: 'none',
+
+
+        hovertext: hancock['Text'],
+
+        lon: hancock['lon'],
+        lat: hancock['lat'],
+        marker: {
+          color: 'blue',
+          reversescale: true,
+          opacity: 0.5,
+          size: 15,
+
+        },
+        text: hancock['Text'],
+        textposition: 'top left'
+    })
+
+    data3 = []
+    data2.push({
+        type: 'scattermapbox',
+        mode: 'markers+text',
+        hoverinfo: 'none',
+
+
+        hovertext: d['Text'],
+
+        lon: d['lon'],
+        lat: d['lat'],
+        marker: {
+          color: 'blue',
+          reversescale: true,
+          opacity: 0.5,
+          size: 15,
+
+        },
+        text: d['Text'],
+        textposition: tpos[0]
+    })
+
+    data3 = []
+
+    data3.push({
+        type: 'scattermapbox',
+        mode: 'markers+text',
+        hoverinfo: 'none',
+        hovertext: '',
+        lon: [h1lon[4]],
+        lat: [h1lat[3]+.0055],
+        z: [20],
+        marker: {
+            color: 'black',
+            reversescale: true,
+            opacity: 0,
+            size: 10,
+        },
+        text: 'Hamline University',
+        textposition: 'center'
+    })
+
+    data2.push( {
+        type: 'choroplethmapbox',
+        locations: ['MN'],
+        coloraxis: 'coloraxis',
+        text: 'Hamline University',
+        textposition: 'bottom',
+
+        z: [10],
+        geojson: {
+            type: "Feature",
+            id: 'MN',
+            geometry: {
+                type: "Polygon",
+                coordinates: hamlineCoord
+            }
+        }
+    })
+
+
+
+
 
     
     layout = {
@@ -79,15 +234,19 @@ d3.csv('RamseyPollingPlaces.csv', function(error, data) {
             family: 'Helvetica',
             size: 12,
             color: '#2222aa'
-          },
+        },
+          coloraxis: {
+            showscale: false,
+            colorscale: [
+                [0, '#9d2235'],
+                [1, '#9d2235']
+            ]
+        },
         dragmode: 'zoom',
         mapbox: {
           center: {
             lat: d['lat'][128],
             lon: d['lon'][128]
-          },
-          bounds: {
-            east: d['lon'][0]
           },
 
           domain: {
@@ -118,7 +277,12 @@ d3.csv('RamseyPollingPlaces.csv', function(error, data) {
         mapboxAccessToken: "pk.eyJ1IjoiYmxyb3NlbmJlcmciLCJhIjoiY2xhMjQ1amkwMDI4eDN3bnlpMW5uOHV2ciJ9.SjZWqAkIvBzadggj9ghVRA"
     })
 
-     Plotly.newPlot("myDiv", data2, layout, {showLink: false, displayModeBar: false});
+     Plotly.newPlot("myDiv", data2, layout, {
+        showLink: false, 
+        displayModeBar: false,
+    }, );
+
+    //Plotly.update("myDiv", layout_update={textposition: tpos})
 
 });
 
